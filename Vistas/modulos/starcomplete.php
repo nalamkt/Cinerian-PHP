14a0f7fc5188f5b3d45bfb68cc1f3b085b8be7d1
nalamkt <131925871+nalamkt@users.noreply.github.com>
2023-06-14 00:05:27 -0300

    Commit June 14th

---
+++ Vistas/modulos/starcomplete.php
@@ -0,0 +1,9 @@
+<svg viewBox="0 0 100 100">
+  <defs>
+    <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
+      <stop offset="0%" stop-color="#FF9900"/>
+      <stop offset="100%" stop-color="#FFDA00"/>
+    </linearGradient>
+  </defs>
+  <polygon points="50,0 61.803,35.355 98.197,35.355 68.405,57.745 79.608,93.098 50,70.708 20.392,93.098 31.595,57.745 1.803,35.355 38.197,35.355" fill="url(#grad)"/>
+</svg>
